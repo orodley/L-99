09ac6df7a369c6ea549eee0479f5f707a613642e
Owen Rodley <Strigoides@gmail.com>
2012-12-13 21:03:27 +1300

    Up to P08

--- L-99.lisp
+++ L-99.lisp
@@ -18,3 +18,38 @@
     (if (zerop i)
       (car list)
       (element-at (cdr list) (1- i)))))
+
+;;; P04
+(defun my-length (list &optional (acc 0))
+  (if (car list)
+    (my-length (cdr list) (1+ acc))
+    acc))
+
+;;; P05
+;;; REALLY inefficient compared to builtin
+(defun my-reverse (list)
+  (if (cdr list)
+    (append (my-reverse (cdr list))
+            (list (car list)))
+    list))
+
+;;; P06
+(defun palindromep (list)
+  (equal list (reverse list)))
+
+;;; P07
+(defun flatten (list)
+  (when list
+    (append (if (listp (car list))
+              (flatten (car list))
+              (list (car list)))
+            (flatten (cdr list)))))
+
+;;; P08
+(defun compress (list)
+  (when list
+    (if (eq (car list)
+            (cadr list))
+      (compress (cdr list))
+      (cons (car list)
+            (compress (cdr list))))))
